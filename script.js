f5a43cec043d0e23d795447fdbf869d41a3b5b5e
SVKalashikam0407 <saketh.kalashikam@gmail.com>
2025-03-30 20:21:02 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+function openSection(sectionId) {
+  document.querySelectorAll('section').forEach(sec => sec.style.display = 'none');
+  document.getElementById(sectionId).style.display = 'block';
+}
